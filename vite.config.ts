86373038d3612f31c9face3324d4354186c061d6
Youva IBRAHIM <ibrahim.y@sfeir.com>
2026-05-28 17:47:33 +0200

    feat: replace dialog with event-based draft injection

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: ".",
-  base: process.env.NODE_ENV === "production" ? "/bella/" : "/",
+  base: "/",
   build: {
     outDir: "dist",
     rollupOptions: {
